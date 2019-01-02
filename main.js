7fc54bc3122ad34ac36f650537c8f084fb8e16e1
Harold Beckerman <halbeckerman@yahoo.com>
2019-01-01 21:33:43 -0600

    functional, but still seems a little choppy

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
-let rightMost;
 let leftBound;
 let rightBound;
 let items = [];
 const slider = document.getElementById('slider');
-const carouselContainer = document.getElementById('carousel-container');
 const imageArray = ["images/image1.jpg", "images/image2.jpg", "images/image3.jpg", "images/image4.jpg", "images/image5.jpg"];
 const scrollLeftButton = document.getElementById('arrow-left');
 const scrollRightButton = document.getElementById('arrow-right');
-let translateWidth;
-let offset = 0;
 
+
+
+//  build image elements and container divs and store in a seperate array
 init = () => {
   imageArray.forEach((x,index)=> {
     let imageDiv = document.createElement('div');
@@ -23,27 +22,32 @@ init = () => {
   items = [...document.querySelectorAll('.item')];
   computeBounds();
 };
-
+//finding width of slider
 function computeBounds() {
   leftBound = items[0].getBoundingClientRect().x;
   rightBound = items[items.length -1].getBoundingClientRect().x;
-  translateWidth = rightBound - leftBound + items[0].getBoundingClientRect().width;
-}
+};
 
 function scrollRight() {
-  let newNode;
-  offset+=translateWidth;
-  items.forEach(x => x.style.transform = `translateX(${offset}px)`);
-  rightMost = items.filter(x => x.getBoundingClientRect().x  > rightBound+100)[0];
 
-  };
+  items.forEach(x=>x.classList.add("animated-right"));
+  rightMost = items.filter(x => x.getBoundingClientRect().x  > rightBound+100)[0];
+  setTimeout(function() {
+  rightMost.remove();
+  slider.prepend(rightMost);
+  items.forEach(x=>x.classList.remove("animated-right"));
+}, 450);
 };
 
 function scrollLeft() {
-  // computeBounds();
-  offset-=translateWidth;
-  items.forEach(x => x.style.transform = `translateX(${offset}px)`);
-}
+  items.forEach(x=>x.classList.add("animated-left"));
+  leftMost = items.filter(x => x.getBoundingClientRect().x  < leftBound-100)[0];
+  setTimeout(function() {
+  leftMost.remove();
+  slider.append(leftMost);
+  items.forEach(x=>x.classList.remove("animated-left"));
+}, 450);
+};
 
 
 scrollLeftButton.addEventListener('click', function() {
